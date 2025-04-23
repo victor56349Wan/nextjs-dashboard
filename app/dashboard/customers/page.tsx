436a6f3a284f93fe7e0c6c9b6b98acc71ff571f7
VicWan <vwan56349@gmail.com>
2025-04-23 11:21:50 +0800

    navigate bwtn pages done

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Customers</h1>
+      <p>Manage your customers here.</p>
+    </div>
+  )
+}
